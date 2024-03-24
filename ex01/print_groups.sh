b8daad4619315c2eb4f2e94bab4c4f779ef1f444
Alejandro Hernandez Hernandez <alejhern@cbr9s5.42barcelona.com>
2024-03-24 23:35:16 +0100

    Actualizaciones

--- ex01/print_groups.sh
+++ ex01/print_groups.sh
@@ -1,2 +1,3 @@
 #/bin/sh
+FT_USER = "daemon"
 groups $FT_USER | tr -d '\n' | sed 's/ /,/g' | sed 's/ ,$//' | tr -d '\n'
---
+++ ex07/r_dwssap.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+# Definir los valores de FT_LINE1 y FT_LINE2
+FT_LINE1=7
+FT_LINE2=15
+
+# Procesar el archivo /etc/passwd
+output=$(cat /etc/passwd | sed '/^#/d' | awk 'NR % 2 == 0' | rev | sort -r | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR <= FT_LINE2' | tr '\n' ',' | sed 's/,$/./')
+
+# Mostrar el resultado
+echo "$output"
